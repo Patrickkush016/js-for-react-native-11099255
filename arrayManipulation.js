2a6ee53897fefa263be79a083e18842db8350374
Patrickkush016 <115562501+Patrickkush016@users.noreply.github.com>
2024-05-18 16:18:57 +0000

    first and second task

---
+++ arrayManipulation.js
@@ -0,0 +1,12 @@
+function processArray(arr) {
+    return arr.map(num => num % 2 === 0 ? num * num : num * 3);
+}
+function formatArrayStrings(stringsArr, processedArr) {
+    return stringsArr.map((str, index) => {
+        return processedArr[index] % 2 === 0 ? str.toUpperCase() : str.toLowerCase();
+    });
+}
+module.exports = {
+    processArray,
+    formatArrayStrings
+};
